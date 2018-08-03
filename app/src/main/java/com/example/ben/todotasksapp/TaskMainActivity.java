78e5d4e4b75a24b0c544aeba3af052cc9e7b24c6
Benard Mathu <mathubenard@gmail.com>
2018-08-03 17:23:19 +0300

    Upload files

---
+++ app/src/main/java/com/example/ben/todotasksapp/TaskMainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ben.todotasksapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TaskMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_main);
+    }
+}
